708e4ea25b00d4a54bf6d30cc6e45cef51a172e3
Lance <tj521.tangj@qq.com>
2019-04-23 11:17:19 +0800

    合并 对照QD版 修改了溯源码申请表

--- src/main/java/com/ningyang/os/action/input/command/web/serve/ApplyCodeCommand.java
+++ src/main/java/com/ningyang/os/action/input/command/web/serve/ApplyCodeCommand.java
@@ -3,6 +3,8 @@ package com.ningyang.os.action.input.command.web.serve;
 import com.ningyang.os.action.input.command.base.ApiCommand;
 import lombok.Data;
 
+import java.util.Date;
+
 /**
  * @Author： kaider
  * @Date：2018/11/15 18:07
@@ -24,4 +26,12 @@ public class ApplyCodeCommand extends ApiCommand {
     private Integer applyCount;
     //申请订单
     private String codeOrder;
+    //溯源码模板
+    private Long codeTemplate;
+    //业务类型  正常业务0 和 简化版业务1
+    private Long businessType;
+    //申请订单-简化版。表示那两笔订单是同一个申请下的数据。
+    private String codeOrderSimplified;
+    //申请时间
+    private Date applyDate;
 }
